67bc9680ead5797cce82c3a7fe609cf7bf7848ed
dennis <desch142@uni-duesseldorf.de>
2020-12-28 20:24:30 +0100

    GFit peak_find peaks (X)

--- GFit.py
+++ GFit.py
@@ -13,28 +13,23 @@ class GFit():
         self.x_err = x_err
         self.y_err = y_err
 
-    def get_peaks_dips(self, number_peaks, stepsize=0.01):
+    def get_peaks_dips(self, number_peaks, stepsize=0.01, max_iter=1000):
         #sort data in x
         p = 0.5
         pks, pks_dict = find_peaks(self.y, prominence=p)
         n = len(pks)
         counter = 1
-        diff = n - number_peaks
-        diff_old=diff
+
+        it_counter = 0
         while n != number_peaks:
-            n_old = n
-            if n_old != n:
-                diff_old = diff
+            it_counter += 1
             diff = n - number_peaks
-            if diff_old == -diff:
+            if it_counter == max_iter:
                 message = "Due to same prominences peaks could not be resolved, peakfind still executed"
                 warnings.warn(message, category=Warning, stacklevel=2)
                 break
-
             p += diff * stepsize
             pks, pks_dict = find_peaks(self.y, prominence=p)
-            if diff == 1 or diff == -1:
-                prominences = pks_dict['prominences']
 
             n = len(pks)
             print(n)
