fec872fe18fc4bf74d52ba3ac03b18d5e1308632
dennis <desch142@uni-duesseldorf.de>
2020-12-28 20:18:11 +0100

    GFit concept

--- GFit.py
+++ GFit.py
@@ -3,6 +3,7 @@ from scipy.optimize import curve_fit
 from scipy import odr
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
+import warnings
 
 
 class GFit():
@@ -11,11 +12,37 @@ class GFit():
         self.y = y
         self.x_err = x_err
         self.y_err = y_err
-        #self.peaks, self.dips = self.get_peaks_dips()
 
-    def get_peaks_dips(self, height, prominence, distance, ):
-        pass
-        #return [peaks, dips]
+    def get_peaks_dips(self, number_peaks, stepsize=0.01):
+        #sort data in x
+        p = 0.5
+        pks, pks_dict = find_peaks(self.y, prominence=p)
+        n = len(pks)
+        counter = 1
+        diff = n - number_peaks
+        diff_old=diff
+        while n != number_peaks:
+            n_old = n
+            if n_old != n:
+                diff_old = diff
+            diff = n - number_peaks
+            if diff_old == -diff:
+                message = "Due to same prominences peaks could not be resolved, peakfind still executed"
+                warnings.warn(message, category=Warning, stacklevel=2)
+                break
+
+            p += diff * stepsize
+            pks, pks_dict = find_peaks(self.y, prominence=p)
+            if diff == 1 or diff == -1:
+                prominences = pks_dict['prominences']
+
+            n = len(pks)
+            print(n)
+            counter += 1
+        print("#####")
+        print(f"{n}/{number_peaks} peaks found")
+        print("#####")
+        return [self.x[pks], self.y[pks]]
 
     def get_guess(self):
         pass
