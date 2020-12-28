2d1e8051147094b68722fd0d70218dc43c0924cd
dennis <desch142@uni-duesseldorf.de>
2020-12-28 18:01:41 +0100

    GFit added

--- GFit.py
+++ GFit.py
@@ -1,9 +1,28 @@
 import numpy as np
+from scipy.optimize import curve_fit
+from scipy import odr
 
 
 class GFit():
     def __init__(self, x, y, x_err, y_err):
+        self.x = x
+        self.y = y
+        self.x_err = x_err
+        self.y_err = y_err
+
+    def get_peaks_dips(self):
         pass
+
+    def get_guess(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def plot_save(self, xlabel, ylabel, title, savename):
+        pass
+
+
 
 
 
