1f865e61937d8217fafdf049cd1f9fb4726d192c
dennis <desch142@uni-duesseldorf.de>
2020-12-29 15:42:47 +0100

    GFit peak_find smoothing added

--- GFit.py
+++ GFit.py
@@ -15,14 +15,24 @@ class GFit():
         self.y_err = y_err
 
     def get_peaks_dips_default(self, number_peaks, window_size=None, smoothing=False, show_smoothing=True,stepsize=0.01, max_iter=1000):
+        """
+        This method aims to find a wanted number of peaks and automatically finds peaks for further computations
+
+        :param number_peaks: int; The number of peaks to find
+        :param window_size: int; A paramater used for the smoothing of the data
+        :param smoothing: boolean; Parameter if smoothing should be applied or not
+        :param show_smoothing: boolean; Parameter if the smoothed data should be shown
+        :param stepsize: float; How much the prominence should be increased/decreased after one iteration
+        :param max_iter: int; Stopping condition for the algorithm, since multiple peaks can have the same prominence and
+        therefore the algorithm sometimes cannot find the wanted number of parameters.
+        :return: returns the found (x,y) data-points for the peaks and dips
+        """
         #sort data in x
         p = 0.5
 
-        if smoothing == True:
-            if window_size == None:
-                window_size = int(len(self.x) * 0.2)
+        if smoothing:
             y_data = self.smoothing(window_size=window_size)
-            if show_smoothing == True:
+            if show_smoothing:
                 plt.plot(self.x, y_data)
         else:
             y_data = self.y
@@ -40,7 +50,7 @@ class GFit():
                 message = "Due to same prominences peaks could not be resolved, peakfind still executed"
                 warnings.warn(message, category=Warning, stacklevel=2)
                 break
-            p += diff * stepsize
+            p += diff * stepsize # change the prominence into the "right" direction
             pks, pks_dict = find_peaks(y_data, prominence=p)
 
             n = len(pks)
@@ -50,6 +60,11 @@ class GFit():
         return [self.x[pks], y_data[pks]]
 
     def smoothing(self, window_size):
+        """
+        This method smooths data using np.convolve. It is useful if the data is ver noisy.
+        :param window_size: int; parameter for the window size of the convolution
+        :return: smoothed data
+        """
         y_data = np.convolve(self.y, np.ones((window_size,)) / window_size, mode='same')
         return y_data
 
